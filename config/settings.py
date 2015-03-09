7e610f719b0bb43a905f345faea0ab0e63fac010
almey <alanmeyerclp@gmail.com>
2015-03-09 16:39:01 +0100

    added settings for submodule

--- config/settings.py
+++ config/settings.py
@@ -10,6 +10,8 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+from transformation.config.settings import *
+
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 
@@ -36,6 +38,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'transformation',
 )
 
 MIDDLEWARE_CLASSES = (
